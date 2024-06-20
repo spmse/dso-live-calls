080451403a3d9751d1a3b8914bc2936a68460d6f
Sven Patrick Meier <sven.patrick.meier@gmail.com>
2024-06-20 19:28:25 +0200

    docs: add live call notes from 2024-06-13

---
+++ some.py
@@ -0,0 +1,6 @@
+# with open("./example.md", "w") as f:
+#     f.write("some dummy text\n")
+#     f.write("some dummy text other\n")
+#     f.write("some dummy text other\n")
+#     f.write("some dummy text other\n")
+print("hello world")
