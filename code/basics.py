71cb2ac2354f5e2b981422e851a95f2a4084e337
Sven Patrick Meier <sven.patrick.meier@gmail.com>
2024-06-27 21:32:09 +0200

    chore: add error handling example

--- code/basics.py
+++ code/basics.py
@@ -3,12 +3,12 @@ from typing import List
 a = ['a', 1, 'b']
 first = a[0]
 last = a[-1]
-print("initial", a)
+# print("initial", a)
 # print out list info
-print("only first element", a[0])
+# print("only first element", a[0])
 a[0] = last  # a[0] -> 'b'
 a[-1] = first
-print("final", a)
+# print("final", a)
 # Generating Lists
 # generate a list of 1000 number elements --> 0 - 1000
 numbers_to_thousand = [y for y in range(1001)]
@@ -46,7 +46,7 @@ def filter_for_fours(number_list):
 
 
 list_of_fours2 = filter_for_fours(range(250000, 6000000))
-print(list_of_fours2)
+# print(list_of_fours2)
 
 # region: filter list for numbers that can be divided by 3
 
@@ -85,6 +85,33 @@ a = filter_for_threes(range(0, 10))
 b = filter_for_threes_alt(range(0, 10))
 c = filter_for_threes_alt2(range(0, 10))
 
-print(a, b, c)
+# print(a, b, c)
 # endregion
 # endregion
+
+# generator function for number lists
+
+
+def generate_number_list_in_interval(upper_bound: int, lower_bound: int = 0) -> List[int]:
+    try:
+        lower_bound = int(lower_bound)
+        upper_bound = int(upper_bound)
+    except:
+        raise Exception("you fucked up")
+
+    if type(upper_bound) == int and type(lower_bound) == int:
+        step = 1
+        try:
+            if upper_bound <= lower_bound:
+                raise ValueError(
+                    "invalid values provided. upper_bound cannot be <= lower_bound")
+        except ValueError:
+            step = -1
+
+        return list(range(lower_bound, upper_bound, step))
+    raise TypeError("Given parameters is no int")
+
+
+# -> [0,9]
+print(generate_number_list_in_interval(lower_bound=900, upper_bound=100))
+print(generate_number_list_in_interval(lower_bound=9, upper_bound=True))
